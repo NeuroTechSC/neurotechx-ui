b8d4a830396a2958e04734984c493c1e0434954e
colperei <colperei@ucsc.edu>
2020-08-29 19:43:31 -0400

    Display random question

--- api/api.py
+++ api/api.py
@@ -1,7 +1,42 @@
 import time
+import random
+from random import randrange
+
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/question', methods=['POST', 'GET'])
+def get_random_question():
+    ran = ['Do you believe in ghosts?',
+                'Have you ever seen a UFO?',
+                'Can cats jump six times their length?',
+                'Do you like chocolate milkshakes?',
+                'Were you in the swamp yesterday?',
+                'Did you see Bigfoot?',
+                'Can you see the moon?',
+                'Do you know how to swim?',
+                'Can you play poker?',
+                'Do you have a twin?',
+                'Were you born in the summer?',
+                'Do you believe in Santa Claus?',
+                'Can you make yourself disappear?',
+                'Were you on Survivor last year?',
+                'Do you know the Schrödinger equation of quantum theory?',
+                'Do mice really eat cheese?',
+                'Is your shoe size 14?',
+                'Can you see out the back of your head?',
+                'Are Martians really green?',
+                'Have elves always live at the North Pole?',]
+
+                # https://teens.lovetoknow.com/teen-activities-things-do/100-random-unexpected-yes-no-questions
+
+    random.shuffle(ran)
+    randQuestion = randrange(20)
+    question = ran[randQuestion]
+
+    return {'question': question}
+
 
 @app.route('/time')
 def get_current_time():
--- src/views/Display.js
+++ src/views/Display.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
-import PropTypes from "prop-types";
-import { Container, Row, Col, Button } from "shards-react";
+import { Container, Button } from "shards-react";
 
+function Display() {
+  const [question, setQuestion] = useState(0);
+  const [currenttime, setCurrentTime] = useState(0);
 
-function App() {
-  const [currentTime, setCurrentTime] = useState(0);
-
+  useEffect(() => {
+    fetch('/question').then(res => res.json()).then(data => { // Request from Flask
+      setQuestion(data.question);
+    });
+  }, []);
   useEffect(() => {
     fetch('/time').then(res => res.json()).then(data => { // Request from Flask
       setCurrentTime(data.time);
@@ -16,32 +20,21 @@ function App() {
     <Container fluid className="main-content-container px-4">
     <div className="App">
       <header className="App-header">
+        <p>{question}</p>
+        <p>Current Time: {currenttime}</p>
 
-
-
-        <p>The current time is {currentTime}.</p>
       </header>
     </div>
-    <div className = 'response'>
+    <div className = 'Next'>
+    <form action="question" method="post">
           <Button theme="primary" className="mb-2 mr-1">
-              Response
-           </Button>
-           </div>
-        </Container>
+              Next
+          </Button>
+    </form>
+    </div>
+
+    </Container>
   );
 }
-// const Display = () => (
-// <Container fluid className="main-content-container px-4">
-//       <div className = 'response'>
-//         <Button theme="primary" className="mb-2 mr-1">
-//           Primary
-//         </Button>
-//       </div>
-//
-//     </Container>
-//
-//   );
 
-
-
-export default App;
+export default Display;
