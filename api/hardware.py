b62db5f26be638e4852569b97c11178439a2ca2e
KhoomeiK <rohan.pandey@gmail.com>
2020-09-23 16:08:34 -0700

    hardwareUp

--- api/hardware.py
+++ api/hardware.py
@@ -7,23 +7,23 @@ from brainflow.board_shim import BoardShim, BrainFlowInputParams
 from brainflow.data_filter import DataFilter, FilterTypes, AggOperations
 
 
-def main ():
-    parser = argparse.ArgumentParser ()
+def main():
+    parser = argparse.ArgumentParser()
     # use docs to check which parameters are required for specific board, e.g. for Cyton - set serial port
-    parser.add_argument ('--timeout', type = int, help  = 'timeout for device discovery or connection', required = False, default = 0)
-    parser.add_argument ('--ip-port', type = int, help  = 'ip port', required = False, default = 0)
-    parser.add_argument ('--ip-protocol', type = int, help  = 'ip protocol, check IpProtocolType enum', required = False, default = 0)
-    parser.add_argument ('--ip-address', type = str, help  = 'ip address', required = False, default = '')
-    parser.add_argument ('--serial-port', type = str, help  = 'serial port', required = False, default = '')
-    parser.add_argument ('--mac-address', type = str, help  = 'mac address', required = False, default = '')
-    parser.add_argument ('--other-info', type = str, help  = 'other info', required = False, default = '')
-    parser.add_argument ('--streamer-params', type = str, help  = 'streamer params', required = False, default = '')
-    parser.add_argument ('--serial-number', type = str, help  = 'serial number', required = False, default = '')
-    parser.add_argument ('--board-id', type = int, help  = 'board id, check docs to get a list of supported boards', required = True)
-    parser.add_argument ('--log', action = 'store_true')
-    args = parser.parse_args ()
+    parser.add_argument('--timeout', type = int, help  = 'timeout for device discovery or connection', required = False, default = 0)
+    parser.add_argument('--ip-port', type = int, help  = 'ip port', required = False, default = 0)
+    parser.add_argument('--ip-protocol', type = int, help  = 'ip protocol, check IpProtocolType enum', required = False, default = 0)
+    parser.add_argument('--ip-address', type = str, help  = 'ip address', required = False, default = '')
+    parser.add_argument('--serial-port', type = str, help  = 'serial port', required = False, default = '')
+    parser.add_argument('--mac-address', type = str, help  = 'mac address', required = False, default = '')
+    parser.add_argument('--other-info', type = str, help  = 'other info', required = False, default = '')
+    parser.add_argument('--streamer-params', type = str, help  = 'streamer params', required = False, default = '')
+    parser.add_argument('--serial-number', type = str, help  = 'serial number', required = False, default = '')
+    parser.add_argument('--board-id', type = int, help  = 'board id, check docs to get a list of supported boards', required = True)
+    parser.add_argument('--log', action = 'store_true')
+    args = parser.parse_args()
 
-    params = BrainFlowInputParams ()
+    params = BrainFlowInputParams()
     params.ip_port = args.ip_port
     params.serial_port = args.serial_port
     params.mac_address = args.mac_address
@@ -36,25 +36,26 @@ def main ():
     print('args', args)
     print('params', params)
 
-    if (args.log):
-        BoardShim.enable_dev_board_logger ()
+    if args.log:
+        BoardShim.enable_dev_board_logger()
     else:
-        BoardShim.disable_board_logger ()
+        BoardShim.disable_board_logger()
 
     board = BoardShim(args.board_id, params)
     print('board', board)
-    board.prepare_session ()
+    board.prepare_session()
+    print('finished preparing session, starting stream')
 
-    # board.start_stream () # use this for default options
-    board.start_stream (45000, args.streamer_params)
-    time.sleep (10)
-    # data = board.get_current_board_data (256) # get latest 256 packages or less, doesnt remove them from internal buffer
-    data = board.get_board_data () # get all data and remove it from internal buffer
-    board.stop_stream ()
-    board.release_session ()
+    # board.start_stream() # use this for default options
+    board.start_stream(500, args.streamer_params)
+    time.sleep(10)
+    # data = board.get_current_board_data(256) # get latest 256 packages or less, doesnt remove them from internal buffer
+    data = board.get_board_data() # get all data and remove it from internal buffer
+    board.stop_stream()
+    board.release_session()
 
-    print (data)
+    print(data)
 
 
 if __name__ == "__main__":
-    main ()
+    main()
