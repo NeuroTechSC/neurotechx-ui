1f44821b0fd3ce4148e39f574313b7a57847d748
beep-boop-stof <loveleenbassi761@gmail.com>
2020-09-23 18:50:33 -0700

    Added 2sec Countdown

--- src/views/Display.js
+++ src/views/Display.js
@@ -12,11 +12,26 @@ function Display() {
     window.location.reload(false);
   }
 
+  //Countdown Timer
+  var timeleft = 2;
+  var downloadTimer = setInterval(function() {
+    if (timeleft <= 0) {
+      clearInterval(downloadTimer);
+      document.getElementById("countdown").innerHTML = "Countdown: Finished";
+    } else {
+      document.getElementById("countdown").innerHTML =
+        "Countdown: " + timeleft + " seconds remaining";
+    }
+    timeleft -= 1;
+  }, 1000);
 
   useEffect(() => {
-    fetch('/time/').then(res => res.json()).then(data => { // Request from Flask
-      setCurrentTime(data.time);
-    });
+    fetch("/time/")
+      .then((res) => res.json())
+      .then((data) => {
+        // Request from Flask
+        setCurrentTime(data.time);
+      });
     fetch("/question/")
       .then((res) => res.json())
       .then((data) => {
@@ -24,8 +39,6 @@ function Display() {
         setQuestion(data[0].question);
       });
   }, []);
-
-
 
   useEffect(() => {
     fetch("/time/")
@@ -36,7 +49,6 @@ function Display() {
       });
   }, []);
 
-
   useEffect(() => {
     fetch("/convertData")
       .then((res) => res.json())
@@ -46,14 +58,13 @@ function Display() {
       });
   }, []);
 
-
-
   return (
     <Container fluid className="main-content-container px-4">
       <div className="introDisplay">
         <h2 style={{ color: "#96C0CE", paddingLeft: "25px" }}>
           Subvocal Recognition
         </h2>
+
         <p style={{ paddingLeft: "55px" }}>
           {" "}
           words words words words words words words words words words words
@@ -80,24 +91,30 @@ function Display() {
         <div class="popup">
           <div className="App" id="mainDisplay">
             <header className="App-header">
-              <p className="Time" id="left">
-                Current Time: {currenttime}
-              </p>
+              <div className="headerPopup">
+                <p className="Time">Current Time: {currenttime}</p>
+                <br></br>
+                <br></br>
+                <br></br>
+                <div id="countdown"></div>
+              </div>
               <br></br>
-              <p className="question-pNew">{question}</p>
-              {/*
+              <div class="questionaire">
+                <p className="question-pNew">{question}</p>
+                {/*
               <a class="ans-yes"  href="#acc">Yes</a>
               <a class="ans-no" href="#acc">No</a>
               */}
 
-              {/*<button action="recordAnswer" className="ans-yesNew" onClick="#acc" >
-                Yes
-              </button>
-              <br></br>
-              <button className="ans-noNew" href="#acc" >
-                No
-            </button>*/}
-
+                <button className="ans-yesNew" onClick="#acc">
+                  Yes
+                </button>
+                <br></br>
+                <button className="ans-noNew" href="#acc">
+                  No
+                </button>
+              </div>
+              {/*
               <a className="ans-yesNew" href="#acc">
                 Yes
               </a>
@@ -106,12 +123,31 @@ function Display() {
               <a className="ans-noNew" href="#acc">
                 No &nbsp;
               </a>
-
+              */}
               <div class=".listContainer"></div>
             </header>
+            {/*id="acc" class="overlay" ....class="popupAcc"*/}
+
+            <div class="accuracy">
+              <a class="close" href="#">
+                {" "}
+                &times;
+              </a>
+              <div>
+                <h1 className="accQ"> Was it accurate?</h1>
+                <a className="ans-yesNew" href="#popupDisplay">
+                  Yes
+                </a>
+                <br></br>
+                <br></br>
+                <a className="ans-noNew" href="#popupDisplay">
+                  No &nbsp;
+                </a>
+              </div>
+            </div>
 
             <div id="next">
-              <div className="popup">
+              <div class="footerPopup">
                 <Button
                   theme="primary"
                   onClick={refreshPage}
@@ -134,44 +170,6 @@ function Display() {
           </div>
         </div>
       </div>
-
-      <div id="acc" class="overlay">
-        <div class="popupAcc">
-          <header>
-            <p className="Time" id="left">
-              Current Time: {currenttime}
-            </p>
-          </header>
-          <a class="close" href="#">
-            {" "}
-            &times;
-          </a>
-          <div>
-            {/*
-            <p className="accuracy" id="right">
-              Accuracy: <input type="radio" name="checkAcc" value="Yes" href="#next"></input>
-              <label for="checkAcc">Yes</label>{" "}
-              <input type="radio" name="checkAcc" value="No" href="#next"></input>
-              <label for="checkAcc">No</label>
-            </p>
-            */}
-            <h1 className="accQ"> Was it accurate?</h1>
-            <a className="ans-yesNew" href="#popupDisplay" value="y">
-              Yes
-            </a>
-            <br></br>
-            <br></br>
-            <a className="ans-noNew" href="#popupDisplay" value="n">
-              No &nbsp;
-            </a>
-          </div>
-          {/*a <class="close" href="#">
-            {" "}
-            &times;
-          </a>*/}
-        </div>
-      </div>
-
     </Container>
   );
 }
