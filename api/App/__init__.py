974ec289099824b35b9b9735b9299d09f9972f75
Hannah McDonald <mcdonaldhannah2000@gmail.com>
2020-09-27 16:34:37 -0700

    Configured for user login.

--- api/App/__init__.py
+++ api/App/__init__.py
@@ -4,7 +4,7 @@ from App.ext import init_ext
 
 from App.settings import envs
 from App.views import init_blueprint
-
+from App.auth import init_bp
 
 def create_app():
     app = Flask(__name__)
@@ -13,7 +13,8 @@ def create_app():
     app.config.from_object(envs.get('develop'))
     # init extensions
     init_ext(app=app)
-    # init blueprint
+    # init blueprints
     init_blueprint(app=app)
+    init_bp(app=app)
 
     return app
--- api/App/ext.py
+++ api/App/ext.py
@@ -1,11 +1,14 @@
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 migrate = Migrate()
+login_manager = LoginManager()
 
 
 def init_ext(app):
     # init db
     db.init_app(app=app)
     migrate.init_app(app=app, db=db)
+    login_manager.init_app(app=app)
