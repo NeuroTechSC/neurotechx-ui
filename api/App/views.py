403ad4a601fb5cc5911fbe338f1afd7c594bd24a
colperei <colperei@ucsc.edu>
2020-09-30 20:25:00 -0700

    Uncommented ML/data

--- api/App/views.py
+++ api/App/views.py
@@ -187,17 +187,16 @@ def record_Subvocalization():
     # # TODO: get serial port from POST
     #
     # # Start recording (2 second chunk..)
-    # chunk = hardware.recordData('/dev/cu.usbserial-DM02582X')
-    # print(chunk.shape)
+    chunk = hardware.recordData('/dev/cu.usbserial-DM02582X')
+    print(chunk.shape)
     #
     # # Data Processing pipeline (2 second chunk..)
-    # chunk = dataProcessing.process(chunk)
-    # print(chunk.shape)
+    chunk = dataProcessing.process(chunk)
+    print(chunk.shape)
     #
     # # ML Model return 1 or 0
-    # prediction = ml.predict(chunk, './ml_model.pt')
-    # print(prediction)
-    prediction = 0
+    prediction = ml.predict(chunk, './ml_model.pt')
+    print(prediction)
     trail_id = request.args.get('questionid')
     print(trail_id)
     db.session.query(ModelResponse).filter(ModelResponse.response_id == trail_id).update(
