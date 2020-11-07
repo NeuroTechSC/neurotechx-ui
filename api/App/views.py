6bd21c299b01898e8bca0797b2e54580766a60fa
colperei <colperei@ucsc.edu>
2020-11-07 15:26:52 -0800

    uncommented

--- api/App/views.py
+++ api/App/views.py
@@ -201,16 +201,16 @@ def record_Subvocalization():
     #
     # # Start recording (2 second chunk..)
     port_number = current_app.config['PORTNUMBER']
-    # chunk = hardware.recordData(port_number)
-    # # chunk = hardware.recordData('/dev/' + PORTNUM)
-    # print(chunk.shape)
-    # # #
-    # # # # Data Processing pipeline (2 second chunk..)
-    # chunk = dataProcessing.process(chunk)
-    # print(chunk.shape)
-    # # #
-    # # # # ML Model return 1 or 0
-    prediction = 0
+    chunk = hardware.recordData(port_number)
+    # chunk = hardware.recordData('/dev/' + PORTNUM)
+    print(chunk.shape)
+    # #
+    # # # Data Processing pipeline (2 second chunk..)
+    chunk = dataProcessing.process(chunk)
+    print(chunk.shape)
+    # #
+    # # # ML Model return 1 or 0
+    # prediction = 0
     trail_id = request.args.get('questionid')
     print(trail_id)
     db.session.query(ModelResponse).filter(ModelResponse.response_id == trail_id).update(
